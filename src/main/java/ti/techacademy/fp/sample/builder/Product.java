4af56739caa94d490932cdb5c928f542dee18cbe
RAFAEL FERNANDEZ <rfernandezo@RAFAELs-MBP.lan>
2021-06-15 15:12:07 -0500

    initial commit

---
+++ src/main/java/ti/techacademy/fp/sample/builder/Product.java
@@ -0,0 +1,11 @@
+package ti.techacademy.fp.sample.builder;
+
+
+// Construir un builder para esta clase
+
+import lombok.Data;
+
+@Data
+public class Product {
+    public boolean expired;
+}
---
+++ src/main/java/ti/techacademy/fp/sample/stream/OfferService.java
@@ -0,0 +1,37 @@
+package ti.techacademy.fp.sample.stream;
+
+import ti.techacademy.fp.sample.repository.OfferrRepository;
+import ti.techacademy.fp.sample.builder.Offer;
+import ti.techacademy.fp.sample.builder.OfferDto;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+public class OfferService {
+
+    OfferrRepository repository;
+    public OfferService(){
+        this.repository = new OfferrRepository();
+    }
+
+    public List<OfferDto> getOffer(){
+
+        return repository.getAll()
+                .stream()
+                .map( mapper())
+                .collect( Collectors.toList());
+    }
+
+    public Function<Offer, OfferDto> mapper(){
+        return (offer) -> {
+            OfferDto dto = new OfferDto();
+            dto.setCode(offer.getCode());
+            dto.setDescription(offer.getDescription());
+            dto.setType(offer.getType()!=null? offer.getType().toUpperCase():"N/A");
+            dto.setChannelName(offer.getChannelName());
+            return  dto;
+        };
+    }
+}
